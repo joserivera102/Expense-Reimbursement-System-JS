c6f01d5ced60bf57766df76c3dae3c5c62414199
Jose Rivera <joseuxs102@gmail.com>
2019-04-23 15:15:08 -0400

    Created user role model

---
+++ src/main/java/com/ers/models/UserRole.java
@@ -0,0 +1,62 @@
+package com.ers.models;
+
+/**
+ * Class used to set up the User Role.
+ * 
+ * @author Jose Rivera
+ *
+ */
+public class UserRole {
+
+	private int id;
+	private String role;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		result = prime * result + ((role == null) ? 0 : role.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		UserRole other = (UserRole) obj;
+		if (id != other.id)
+			return false;
+		if (role == null) {
+			if (other.role != null)
+				return false;
+		} else if (!role.equals(other.role))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "UserRole [id=" + id + ", role=" + role + "]";
+	}
+}
