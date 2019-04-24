9f72ad78060c3f47a5078532bab171dbc6171afb
Jose Rivera <joseuxs102@gmail.com>
2019-04-24 11:56:20 -0400

    Added validation for front end registration form

--- src/main/webapp/js/register.js
+++ src/main/webapp/js/register.js
@@ -22,6 +22,8 @@ function configureRegister() {
  */
 async function registerUser() {
 
+    console.log('in registerUser()');
+
     // Validate the fields
     if (fieldsValid()) {
         // TODO
@@ -37,26 +39,43 @@ async function registerUser() {
  */
 function fieldsValid() {
 
-    // Empty value checks
-    if (document.getElementById('register-fn').value == '') {
-        // TODO
+    // Gather all the fields into an array
+    let fieldsArr = new Array();
+    fieldsArr[0] = document.getElementById('register-fn').value;
+    fieldsArr[1] = document.getElementById('register-ln').value;
+    fieldsArr[2] = document.getElementById('register-email').value;
+    fieldsArr[3] = document.getElementById('register-username').value;
+    fieldsArr[4] = document.getElementById('register-password').value;
+
+    // Loop through and validate for empty values
+    for (let i = 0; i < fieldsArr.length; i++) {
+        if (fieldsArr[i] == '')
+            return false;
     }
 
-    if (document.getElementById('register-ln').value == '') {
-        // TODO
-    }
+    /*
+        Taken from StackOverflow user 'Jaymon' ( 04/24/2019 ), regular expression 
+        used for simple email validation. 
+    */
+    let emailRegex = /\S+@\S+\.\S+/;
+    if (emailRegex.test(fieldsArr[2]) == false)
+        return false;
 
-    if (document.getElementById('register-email').value == '') {
-        // TODO
-    }
+    /*
+        Taken from StackOverflow user 'Jason McCreary' ( 04/24/2019 ), regular expression 
+        used for simple username validation. 
+    */
+    let usernameRegex = /^[a-zA-Z0-9]+$/;
+    if (usernameRegex.test(fieldsArr[3]) == false)
+        return false;
 
-    if (document.getElementById('register-username').value == '') {
-        // TODO
-    }
-
-    if (document.getElementById('register-password').value == '') {
-        // TODO
-    }
+    /*
+        Taken from StackOverflow user 'Minko Gechev' ( 04/24/2019 ), regular expression 
+        used for simple password validation. 
+    */
+    let passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/;
+    if (passwordRegex.test(fieldsArr[4]) == false)
+        return false;
 
     return true;
 }
